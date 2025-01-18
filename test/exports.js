2d366da82e3678b0a5177f30e78633bccd53a3c8
coderiaser <coderaiser@cloudcmd.io>
2025-01-18 20:51:58 +0200

    fix: flatlint: exports

--- test/exports.js
+++ test/exports.js
@@ -1,6 +1,6 @@
 import {test} from 'supertape';
 
-test('flatlint: exports', async (t) => {
+test('flatlint: exports: /with-plugins', async (t) => {
     const {lint, plugins} = await import('flatlint/with-plugins');
     const result = {
         lint,
@@ -11,6 +11,18 @@ test('flatlint: exports', async (t) => {
         ...await import('#flatlint'),
         ...await import('../lib/plugins.js'),
     };
+    
+    t.deepEqual(result, expected);
+    t.end();
+});
+
+test('flatlint: exports', async (t) => {
+    const {lint} = await import('flatlint');
+    const result = {
+        lint,
+    };
+    
+    const expected = await import('#flatlint');
     
     t.deepEqual(result, expected);
     t.end();
