80bf9000bf890cbe0796e2fbba9a172baf2e8f9d
coderiaser <coderaiser@cloudcmd.io>
2025-02-15 13:20:22 +0200

    feature: convert-semicolon-to-comma: class (#1)

---
+++ lib/plugins/convert-semicolon-to-comma/fixture/class.ts
@@ -0,0 +1,7 @@
+export class DrawCircle {
+    #canvas: HTMLElement | SVGElement | null;
+    el: HTMLElement;
+    circle: SVGCircleElement;
+    radius: number;
+    options: SpeccerOptionsInterface;
+}
